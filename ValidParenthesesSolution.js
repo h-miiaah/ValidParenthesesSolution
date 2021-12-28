2d42c26ee6ac1d191008fad9e4bf519dd573ccfa
h-miiaah <haim.miiaah5@gmail.com>
2021-12-27 21:28:54 -0500

    conditionals added

--- ValidParenthesesSolution.js
+++ ValidParenthesesSolution.js
@@ -9,5 +9,12 @@
         "]": "["
     }
     let stack = [];
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "(" || s[i] === "[" || s[i] === "{") {
+            stack.push(s[i]);
+        } else if (stack[stack.length-1] === map[s[i]]) {
+            stack.pop()
+        } else return false;
+    }
    };
    
